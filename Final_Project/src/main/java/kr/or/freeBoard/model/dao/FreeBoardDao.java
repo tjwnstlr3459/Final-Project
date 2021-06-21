d40063b501b689978e3b6e3ee82a67f6b62c7f86
bom1boyy <dudqja8546@naver.com>
2021-06-21 16:06:32 +0900

    freeBoard초기작업

---
+++ Final_Project/src/main/java/kr/or/freeBoard/model/dao/FreeBoardDao.java
@@ -0,0 +1,11 @@
+package kr.or.freeBoard.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FreeBoardDao {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+}
