2a03c056fbf5af45dba943966e080a3769827458
SeoJunsik <tjwnstlr3459@naver.com>
2021-06-21 23:33:51 +0900

    날라간거 복구 및 게시물등록(미구현)

--- Final_Project/src/main/java/kr/or/club/controller/ClubController.java
+++ Final_Project/src/main/java/kr/or/club/controller/ClubController.java
@@ -1,6 +1,13 @@
 package kr.or.club.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.or.club.model.service.ClubService;
 import kr.or.club.model.vo.Board;
@@ -20,6 +28,7 @@ public class ClubController {
 	@Autowired
 	private ClubService service;
 	
+	// 회원이 속한 모임에 클럽게시물 추출
 	@RequestMapping(value = "/myClub.do")
 	public String myClub(@SessionAttribute(required = false) Member m,Model model){
 		ArrayList<Club> clubList = service.memberClubList(m);
@@ -32,6 +41,7 @@ public class ClubController {
 		return "club/myClub";
 	}
 	
+	// 회원이 속한 모임에 클럽게시물 상세출력
 	@ResponseBody
 	@RequestMapping(value = "/photoMore.do")
 	public ArrayList<Board> photoMore(@SessionAttribute(required = false) Member m,Model model, int start) {
@@ -40,5 +50,102 @@ public class ClubController {
 		model.addAttribute("listMore",list);
 		return list;
 	}
+	
+	//임시 게시물 등록페이지
+	@RequestMapping(value = "/insertPostWrite.do")
+	public String insertPostWrite() {
+		return "club/insertPostWrite";
+	}
+	
+	
+	//게시물 등록
+	@RequestMapping(value="/insertPost.do")
+   public String boardWrite(Board b, MultipartFile files[], HttpServletRequest request, Model model) {
+      //파일 목록을 저장할 리스트 생성
+      //같은 클래스명이라 밑에 처럼 길게 나옴
+      ArrayList<Board> fileList = new ArrayList<Board>();
+      
+      System.out.println(b.getBoardContent());
+      System.out.println(b.getBoardCg());
+      System.out.println(b.getBoardTitle());
+      System.out.println(b.getBoardWriter());
+      
+      //파일이 존재하지 않더라도 배열은 무조건 길이 1을 가짐
+      if(files[0].isEmpty()) { //첫번째 타입이 비어있는지 확인
+    	  //첨부파일이 없는 경우
+    	  
+      }else {
+    	  //첨부파일이 있는경우
+    	  //파일처리
+          //getRealPath() -> webapp 폴더
+          String savePath = request.getSession()
+        		  					.getServletContext()
+        		  					.getRealPath("/resources/fileupload/post/");
+          
+          //파일이 1~여러개라 for문으로 묶어준다
+          for(MultipartFile file : files) {
+        	  
+        	//실제 유저가 올린 파일명(filename)
+              String filename = file.getOriginalFilename();
+              //유저가 올린 파일명을 마지막  . 기준으로 분리 test.txt -> test / .txt 로 나누고 겹치면 넘버링을 해주는 형식
+              // indexof : .위치 추출 / substring : begin ~ end 까지 자르기
+              //0부터 filename.indexOf(".") ""안에있는 것까지 onlyFilename에 집어넣어라
+              String onlyFilename = filename.substring(0 , filename.indexOf(".")); 
+              String extention = filename.substring(filename.indexOf("."));// .txt 
+              String filepath = null;
+              int count = 0;
+              
+              // 중복이름 필터
+              while(true) {
+                 if(count == 0) {
+                    filepath = onlyFilename + extention; //test.txt
+                 }else {
+                    filepath = onlyFilename + "_" + count + extention; //test_카운트 숫자.txt
+                 }
+                 File checkFile = new File(savePath+filepath);
+                 if(!checkFile.exists()) { //존재하지 않으면 
+                    break;
+                 }
+                 count++;
+              }
+              Board f = new Board();
+              f.setFileName(filename);
+              f.setFilePath(filepath);
+              fileList.add(f);
+              
+              System.out.println("filename : "  +filename);
+              System.out.println("filepath : "  +filepath);
+              
+              try {
+            	 //스트림 생성
+                 FileOutputStream fos = new FileOutputStream(new File(savePath+filepath)); // 경로 + 파일명
+                 //속도개선 보조 스트림
+                 BufferedOutputStream bos = new BufferedOutputStream(fos);
+                 //bos -> byte타입 벼환
+                 byte[] bytes = file.getBytes(); //try/catch 추가 설정
+                 bos.write(bytes);
+                 bos.close();
+                 
+              } catch (FileNotFoundException e) {
+                 // TODO Auto-generated catch block
+                 e.printStackTrace();
+              } catch (IOException e) {
+                 // TODO Auto-generated catch block
+                 e.printStackTrace();
+              }
+          }
+       }
+      //DB에 넣기
+      int result = service.insertPost(b,fileList);
+      if(result != -1 && result == fileList.size()) {
+    	  model.addAttribute("msg","등록성공");
+      }else {
+    	  model.addAttribute("msg","등록실패");
+      }
+      model.addAttribute("loc","/myClub.jsp");
+      
+      
+      return "common/msg";
+   }
 	
 }
--- Final_Project/src/main/java/kr/or/club/model/dao/ClubDao.java
+++ Final_Project/src/main/java/kr/or/club/model/dao/ClubDao.java
@@ -16,17 +16,20 @@ import kr.or.member.model.vo.Member;
 public class ClubDao {
 	@Autowired
 	private SqlSessionTemplate session;
-
+	
+	//회원이 가입한 클럽들 출력
 	public List<Club> memberClubList(Member m) {
 		List list = session.selectList("club.memberClubList",m);
 		return list;
 	}
-
+	
+	//게시물 총출력
 //	public List<Board> memberClubPosts(Member m) {
 //		List list = session.selectList("club.memberClubPosts",m);
 //		return list;
 //	}
 
+	//더보기로 5개씩 출력하기
 	public List<Board> morePhoto(int start, int end, Member m) {
 		HashMap<String,Object> map = new HashMap<String,Object>();
 		map.put("start", start);
@@ -36,12 +39,52 @@ public class ClubDao {
 		List<Board> list = session.selectList("club.phtoMore",map);
 		return list;
 	}
-
+	
+	//전체게시물 수 확인
 	public int totalCount(Member m) {
 		return session.selectOne("club.totalCount",m);
+	}
+	
+	//게시물 등록
+	public int insertBoard(Board b) {
+		return session.update("club.insertPost",b);
+	}
+	
+	//게시물 등록시 board_no를 알아내기위한 메소드
+	public int selectBoardNo() {
+		return session.selectOne("club.selectBoardNo");
+	}
+
+	public int insertFile(Board f) {
+		return session.update("club.insertFile",f);
 	}
 	
 	
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
