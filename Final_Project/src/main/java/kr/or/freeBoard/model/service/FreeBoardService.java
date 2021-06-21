fb1292c8e86e3f09ad282c0dce4c8d519c87771d
bom1boyy <dudqja8546@naver.com>
2021-06-21 18:42:01 +0900

    asdsad

---
+++ Final_Project/src/main/java/kr/or/freeBoard/model/service/FreeBoardService.java
@@ -0,0 +1,9 @@
+package kr.or.freeBoard.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FreeBoardService {
+
+}
