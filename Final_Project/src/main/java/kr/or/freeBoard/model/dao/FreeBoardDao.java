406451c1aa2a2f46185b35af93ad153150f86b9a
bom1boyy <dudqja8546@naver.com>
2021-06-22 17:09:02 +0900

    프리보드 등록 화면구성ㄱㄱ

--- Final_Project/src/main/java/kr/or/freeBoard/model/dao/FreeBoardDao.java
+++ Final_Project/src/main/java/kr/or/freeBoard/model/dao/FreeBoardDao.java
@@ -4,8 +4,15 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import kr.or.freeBoard.model.vo.FreeBoard;
+
 @Repository
 public class FreeBoardDao {
 	@Autowired
-	private SqlSessionTemplate sqlSession; 
+	private SqlSessionTemplate sqlSession;
+
+	public int insertFreeBoard(FreeBoard fb) {
+		int result = sqlSession.insert("insertFreeBoard",fb);
+		return result;
+	} 
 }
