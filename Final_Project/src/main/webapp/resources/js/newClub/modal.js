d31c614e36885eb67454bbab537f5b632e4bb059
wogur <wogur@DESKTOP-J9A6P8G>
2021-06-22 14:37:11 +0900

    커밋할거야...!

--- Final_Project/src/main/webapp/resources/js/newClub/modal.js
+++ Final_Project/src/main/webapp/resources/js/newClub/modal.js
@@ -1,10 +1,8 @@
 $( document ).ready(function() {
   $('.trigger a').on('click', function() {
-  	console.log("aaa");
      $('.modal-wrapper').toggleClass('open');
     $('.page-wrapper').toggleClass('blur-it');
      return false;
-    }); 
+    });
     });
     
-
