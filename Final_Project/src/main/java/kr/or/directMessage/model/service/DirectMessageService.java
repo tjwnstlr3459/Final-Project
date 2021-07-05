dd41cab5a3bbae92c96a8e83aadeaf4ba11b83ba
palcimer <palcimer@gmail.com>
2021-07-06 00:08:27 +0900

    DM 내비게이션ing

--- Final_Project/src/main/java/kr/or/directMessage/model/service/DirectMessageService.java
+++ Final_Project/src/main/java/kr/or/directMessage/model/service/DirectMessageService.java
@@ -37,10 +37,66 @@ public class DirectMessageService {
 		return dao.insertMultiDm(dm,memberNo);
 	}
 
-	public DirectMessageData selectDmByName(String memberNick) {
+	public DirectMessageData selectDmByName(String memberNick, int index) {
 		DirectMessageData dmData = new DirectMessageData();
-		dmData.setDmList((ArrayList<DirectMessage>)dao.selectDmByName(memberNick));
-		dmData.setUnread(dao.selectUnreadDm(memberNick));
+		System.out.println(index);
+		
+		int paging = 5;
+		int dataPerPage = 10;
+		int end = index * dataPerPage;
+		int start = end - dataPerPage + 1;
+		
+		HashMap<String, String> param = new HashMap<String, String>();
+		param.put("start", Integer.toString(start));
+		param.put("end", Integer.toString(end));
+		param.put("memberNick", memberNick);
+		System.out.println(param.get("start"));
+		System.out.println(param.get("end"));
+		
+		
+		
+		dmData.setUnreadDmList((ArrayList<DirectMessage>)dao.selectUnreadDmByName(param));
+		dmData.setDmList((ArrayList<DirectMessage>)dao.selectDmByName(param));
+		System.out.println(dmData.getDmList().size());
+		int unreadDm = dao.selectUnreadDm(memberNick);
+		dmData.setUnread(unreadDm);
+		int allDm = dao.selectAllDmCount(memberNick);
+		System.out.println(allDm);		
+		
+		//전체 DM 페이지 내비게이션
+		int allTotalNaviPage = allDm % dataPerPage == 0 ? allDm / dataPerPage : allDm / dataPerPage + 1;
+		
+		int allNavi = ((index - 1) / paging) * paging + 1;
+		String allDmNavigation = "<div class='paging'>";
+		// 이전버튼 생성 여부
+		if (allNavi != 1) {
+			allDmNavigation += "<a href='javascript:void(0)' onclick='readPaging(" + (index - paging) + ")'>" + "&#60;</a>";
+		} else {
+			allDmNavigation += "<div class='pageDisabled'>&#60;</div>";
+		}
+		// 1~5단위 페이지 생성
+		for (int i = 0; i < paging; i++) {
+			// 사용자가 클릭해서 보고있는 페이지인 경우 효과
+			if (allNavi == index) {
+				allDmNavigation += "<div class='pageThis'>" + allNavi + "</div>";
+			} else {
+				allDmNavigation += "<a href='javascript:void(0)' onclick='readPaging(this)'>" + allNavi + "</a>";
+			}
+			// 시작된 페이지 네비게이션 navi 증가 > 1,2,3,4,5 / 6,7,8,9,10 / .....
+			allNavi++;
+			if (allNavi > allTotalNaviPage) {
+				break;
+			}
+		}
+		// 다음버튼 생성 여부
+		if (allNavi <= allTotalNaviPage) {
+			allDmNavigation += "<a href='javascript:void(0)' onclick='readPaging(" + (index + paging) + ")'>" + "&#60;</a>";
+		} else {
+			allDmNavigation += "<div class='pageDisabled'>&#60;</div>";
+		}
+		allDmNavigation += "</div>";
+		
+		dmData.setAllPaging(allDmNavigation);
 		return dmData;		
 	}
 	
--- Final_Project/src/main/java/kr/or/directMessage/model/vo/DirectMessageData.java
+++ Final_Project/src/main/java/kr/or/directMessage/model/vo/DirectMessageData.java
@@ -7,5 +7,8 @@ import lombok.Data;
 @Data
 public class DirectMessageData {
 	private ArrayList<DirectMessage> dmList;
+	private ArrayList<DirectMessage> unreadDmList;
 	private int unread;
+	private String unreadPaging;
+	private String allPaging;
 }
