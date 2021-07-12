b8ec8ebada5bf36d210d1aa761761f21b9bfbd6e
kimminhyeong <kimminhyeong@218.38.137.28>
2021-07-12 14:58:12 +0900

    MH - 웹소켓 변경 커밋

--- Final_Project/src/main/webapp/resources/js/admin/adminWebSocket.js
+++ Final_Project/src/main/webapp/resources/js/admin/adminWebSocket.js
@@ -4,8 +4,7 @@ $(function(){
 	function initMember(){
 		//memberEmail = email;
 		//웹 소켓 생성 > 연결 시도
-		//ws = new WebSocket("ws:/192.168.219.105/inMemberCount.do");
-		ws = new WebSocket("ws://khdsa1.iptime.org:18080/chat.do");
+		ws = new WebSocket("ws://khdsa1.iptime.org:18080/inMemberCount.do");
 		//1. 웹소켓 연결 성공 시 실행 함수 지정
 		ws.onopen = inMember;
 		//2. 웹소켓으로 서버가 데이터를 전송할 시 로직을 수행할 함수 지정
@@ -33,23 +32,7 @@ $(function(){
 	function outMember(){
 	
 	}
-	
-	//접속 회원 및 채팅 요청용
-	function initChat() {
-		var email = "<c:out value='${m.email}'/>";
-		if(!email == undefined) {
-			//ws = new WebSocket("ws:/192.168.10.20/inMemberCount.do");
-			ws = new WebSocket("ws://khdsa1.iptime.org:18080/chat.do");
-			//1. 웹소켓 연결 성공 시 실행 함수 지정
-			ws.onopen = inMember;
-			//2. 웹소켓으로 서버가 데이터를 전송할 시 로직을 수행할 함수 지정
-			ws.onmessage = receiveMsg;
-			//3. 웹소켓연결이 종료되면 수행할 함수 지정
-			ws.onclose = outMember;
-		}		
-	}
-	//접속 회원 및 채팅 요청용
-	
+
 	initMember();		//우리 사이트 접속(전역) 시 웹소켓 연결 함수
 	insertVisit();		//우리 사이트 접속(전역) 시 visit테이블에 사용자 ip 저장(오늘 날짜에 없을 시에만)
 });
